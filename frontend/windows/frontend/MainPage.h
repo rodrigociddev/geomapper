c30220ad91cf8a61ca6865abf07dc88441e18194
Rodrigo Lopes <44005400+rodrigociddev@users.noreply.github.com>
2024-09-20 15:58:00 -0400

    Frontend setup commit

---
+++ frontend/windows/frontend/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::frontend::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::frontend::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
