4cc8dd7ad0677010271197497775b5dda68a5c80
Christopher Alfonso <123342898+ChrisA27300@users.noreply.github.com>
2024-11-14 16:32:28 -0500

    Update MediaService.java
    fix to rename media

--- backend/src/main/java/com/example/geodemo/media/MediaService.java
+++ backend/src/main/java/com/example/geodemo/media/MediaService.java
@@ -21,25 +21,38 @@ public class MediaService {
         this.projectService = projectService;
     }
 
-    public String renameMedia(String oldName, String newName){
+    public String renameMedia(String oldName, String newName) {
         Media media = projectService.searchMedia(oldName);
-        if(projectService.searchMedia(newName)!= null){
+        if (projectService.searchMedia(newName) != null) {
             System.out.println("Media already exists exception");
             throw new MediaAlreadyExistsException(newName);
         }
-        if(media == null){
+        if (media == null) {
             System.out.println("Media named " + oldName + " does not exist");
             throw new MediaNotFoundException(oldName);
+        }
 
+        //defining the directory for user media files
+        String workingDir = System.getProperty("user.dir");
+        File oldFile = new File(workingDir + File.separator + "userMedia" + File.separator + oldName);
+        File newFile = new File(workingDir + File.separator + "userMedia" + File.separator + newName);
+
+        if (oldFile.exists()) {
+            boolean renamed = oldFile.renameTo(newFile);
+            if (!renamed) {
+                throw new RuntimeException("Failed to rename file on the filesystem.");
+            }
+        } else {
+            System.out.println("File not found in userMedia folder: " + oldName);
+            throw new MediaNotFoundException(oldName);
         }
 
         project.deleteMedia(media);
         media.setName(newName);
         project.addMedia(media);
-        System.out.println("Changed " + oldName + " Into " + newName);
-        return "Media name succesfully changed";
 
-
+        System.out.println("Changed " + oldName + " into " + newName);
+        return "Media name successfully changed";
     }
 
     public String addAnnotations(String mediaName,String annotation){
